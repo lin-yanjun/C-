ec52d3d500daa4d5af900f4018a490b193ab2e3d
shimmer <1462800446@qq.com>
2024-01-13 16:39:11 +0800

    修改入参格式

--- chapter02/06_BooleanTeat.c
+++ chapter02/06_BooleanTeat.c
@@ -14,10 +14,8 @@ int main(){
     float x1 = 2 , y1 = 2 , x2 = 2 , y2 = -2 ;
     float x3 = -2 , y3 = 2 , x4 = -2 , y4 = -2 ;
     float x , y ;
-    printf("请输入坐标xy:\n");
-    scanf("%f %f" , &x , &y);
-//    printf("请输入坐标y:\n");
-//    scanf("请输入坐标y：" ,  );
+    printf("请输入坐标(x,y):");
+    scanf("%f,%f" , &x , &y);
     float d1 , d2 , d3 , d4 ;
     d1 = (x1 - x) * (x1 - x) + (y1 - y) * (y1 - y);
     d2 = (x2 - x) * (x2 - x) + (y2 - y) * (y2 - y);
@@ -25,9 +23,9 @@ int main(){
     d4 = (x4 - x) * (x4 - x) + (y4 - y) * (y4 - y);
 
     if(d1 > 1 && d2 > 1 && d3 > 1 && d4 > 1){
-        printf("坐标（%f %f）的高度为0" , x , y);
+        printf("坐标（%.2f %.2f）的高度为0" , x , y);
     } else{
-        printf("坐标（%f %f）的高度为%d" , x , y , h);
+        printf("坐标（%.2f %.2f）的高度为%d" , x , y , h);
     }
 
     return 0;
